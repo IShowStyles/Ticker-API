f9e3264b3084fdac4957467f94698ab1dc292fd5
ALEXSANDER ZHOLUDEV <zholudev111@gmail.com>
2025-04-03 21:02:10 +0300

    addded fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { GraphQLModule } from '@nestjs/graphql';
+// import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+// import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { TickerModule } from './ticker/ticker.module';
 import { RedisModule } from './redis/redis.module';
-import { ConfigModule } from '@nestjs/config';
+// import { ConfigModule } from '@nestjs/config';
 import { AppResolver } from './app.resolver';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    // ConfigModule.forRoot(),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
@@ -21,8 +21,8 @@ import { AppResolver } from './app.resolver';
       },
       context: ({ req, res }) => ({ req, res }),
     }),
-    TickerModule,
-    RedisModule,
+    // TickerModule,
+    // RedisModule,
   ],
   controllers: [AppController],
   providers: [AppService, AppResolver],
